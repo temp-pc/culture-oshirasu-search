5ddb9ecaa12a190bb779d82e774ccea45356bb5a
temp <temppc42@gmail.com>
2023-08-24 12:45:47 +0900

    setting button test

--- script.js
+++ script.js
@@ -279,3 +279,11 @@ displayMyListButton.addEventListener('click', function () {
   filterLinks(displayMyListButtonPressed);
   clearFilterElements();
 })
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+  container: 'body'
+})
